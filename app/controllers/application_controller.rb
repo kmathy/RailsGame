5bd146d1d94fa16c12de8f9d1175cba0f88c898e
kmathy <kevin.mathy95@gmail.com>
2016-02-14 16:57:26 +0100

    petit bloc pour utilisateur

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :logged_in?
+  before_filter :current_user
+
+  def current_user
+    if session['warden.user.user.key']
+      @current_user ||= User.find_by_id(session['warden.user.user.key'][0][0])
+    end
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
 end
