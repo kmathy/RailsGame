2804fa763791d4fc82976fdb516fcaa629449151
kmathy <kevin.mathy95@gmail.com>
2016-03-05 17:12:04 +0100

    heroku

--- db/migrate/20160216161632_create_table_matches.rb
+++
@@ -1,7 +0,0 @@
-class CreateTableMatches < ActiveRecord::Migration
-  def change
-    create_table :matches
-    add_column :matches, :score, :integer
-    add_column :matches, :points, :integer
-  end
-end
